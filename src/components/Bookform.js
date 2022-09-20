e03b0a77305ab2e55cef36f4037b5d67acfcce59
Suzi216 <66145033+Suzi216@users.noreply.github.com>
2022-09-20 08:50:16 +0200

    Adding linters

--- src/components/Bookform.js
+++ src/components/Bookform.js
@@ -1,7 +1,9 @@
+import { addBook } from '../redux/books/books';
+
 const Bookform = () => (
   <>
     <h4> ADD NEW BOOK</h4>
-    <form className="form-container">
+    <form onSubmit={addBook} className="form-container">
       <input type="text" name="name" placeholder="Book title" />
       <input type="text" name="name" placeholder="Author" />
       <input type="submit" value="Add Book" />
